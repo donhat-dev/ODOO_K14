fbee7e07fdaf7f1111b82e92fefb0d39046c6422
donhat-dev <donhat.hn@gmail.com>
2025-12-18 21:40:43 +0700

    Add training order controller and template for order details management

--- addons/training_center/__manifest__.py
+++ addons/training_center/__manifest__.py
@@ -12,7 +12,8 @@
         "views/training_course_view.xml",
         "views/training_order_line_views.xml",
         "views/training_order_options_wizard_views.xml",
-        "views/training_order_view.xml"
+        "views/training_order_view.xml",
+        "views/templates.xml",
     ],
     "installable": True,
     "application": False,
---
+++ addons/training_center/controllers/main.py
@@ -0,0 +1,113 @@
+from odoo import http
+from odoo.http import request
+from odoo.http import Response
+import json
+
+class TrainingCenterController(http.Controller):
+
+    @http.route(
+        '/training_order/<int:order_id>/details',
+        type='http',
+        auth='public',
+        methods=['GET'],
+        cors='*',
+        csrf=False,
+    )
+    def details_training_order(self, order_id, **kwargs):
+        order = request.env['training.order'].sudo().search([('id', '=', order_id)], limit=1)
+
+        data = order.read(['id', 'display_name'])
+
+        return request.render(
+            'training_center.training_order_template',
+            {
+                'orders_data': data,
+            }
+        )
+
+    @http.route(
+        '/training_order/<int:order_id>',
+        type='http',
+        auth='public',
+        methods=['GET'],
+        cors='*',
+        csrf=False,
+    )
+    def training_order(self, order_id, **kwargs):
+        # params = request.httprequest.params
+        fields = kwargs.get('fields', 'id,display_name')
+
+        order = request.env['training.order'].sudo().search([('id', '=', order_id)], limit=1)
+
+        data = order.read(fields.split(','))
+
+        return request.make_json_response(
+            data=data,
+            status=200
+        )
+    
+    @http.route(
+        '/training_order/<int:order_id>',
+        type='http',
+        auth='public',
+        methods=['PUT'],
+        cors='*',
+        csrf=False,
+    )
+    def update_training_order(self, order_id, **kwargs):
+
+        order = request.env['training.order'].sudo().search([('id', '=', order_id)], limit=1)
+        update_data = {}
+        fields_list = order.fields_get().keys()
+        for key, value in kwargs.items():
+            if key not in fields_list:
+                continue
+            update_data[key] = value
+        
+        order.sudo().write(update_data)
+
+        return request.make_json_response(
+            data="Success",
+            status=200
+        )
+    
+
+    @http.route(
+        '/training_order',
+        type='http',
+        auth='public',
+        methods=['POST'],
+        cors='*',
+        csrf=False,
+    )
+    def create_training_order(self):
+        try:
+            data = request.httprequest.data
+
+            if not data:
+                return request.make_json_response(
+                    data="No data provided",
+                    status=400
+                )
+
+            body = json.loads(data)
+
+            training_order_obj = request.env['training.order'].sudo()
+
+            fields_list = training_order_obj.fields_get().keys()
+            create_data = {}
+            for key, value in body.items():
+                if key in fields_list:
+                    create_data[key] = value
+            
+            new_order = training_order_obj.sudo().create(create_data)
+
+            return request.make_json_response(
+                data={"id": new_order.id},
+                status=200
+            )
+        except Exception as e:
+            return request.make_json_response(
+                data=str(e),
+                status=500
+            )
